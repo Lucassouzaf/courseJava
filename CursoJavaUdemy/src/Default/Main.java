00fb18811fbaf274673fcb4147db12c859b6e4ca
Lucas Fernando <lucas.fernando@pixmidia.com.br>
2024-07-25 15:29:31 -0300

    Second commit, ADC projeto

---
+++ CursoJavaUdemy/src/Default/Main.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+package Default;
+
+public class Main {
+	
+		public static void main (String[] args) {
+			
+			int x = 5;
+			
+			System.out.println("Bom dia");
+			
+			if (x < 0) {
+				System.out.println("Boa tarde");
+			}
+			else {
+			
+			System.out.println("Boa noite");
+		
+			}
+		}
+}
